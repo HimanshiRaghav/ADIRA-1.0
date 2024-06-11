62cc05bc43f67995e23d95791e5ff76ee1c3d12b
HimanshiRaghav <142144235+HimanshiRaghav@users.noreply.github.com>
2024-06-11 18:52:02 +0530

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode:false,
+    images: {
+      domains: ['shorturl.at',"media.licdn.com","encrypted-tbn0.gstatic.com","lh3.googleusercontent.com"],
+    },
+  };
+  
+  export default nextConfig;
+  
